96b2c6f39b7cbbea3a6e26a1790d0dbb89e047ef
Tero Paloheimo <tero.paloheimo@iki.fi>
2015-10-25 17:22:49 +0200

    Added the MIT license text to source file headers

--- encryption.py
+++ encryption.py
@@ -1,11 +1,31 @@
 # i!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-
 # Encryption and decryption
 
-# MIT License
-# FIXME: Insert full license text here
+# The MIT License (MIT)
+
+# Copyright (c) 2015 Antti Nilakari
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Authors
 # Antti Nilakari <antti.nilakari@gmail.com>
 
 
@@ -129,7 +149,6 @@ def encrypt_group(group, key, row, col):
 
 
 def encrypt(plaintext, key):
-
     # Strip spaces and pack into groups
     plaintext = "".join(plaintext.split())
     plaintext_groups = [plaintext[i:i + 5]
